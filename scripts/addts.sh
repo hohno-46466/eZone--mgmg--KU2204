2611a53dfe232d603f49309ef97e6e7026dee9f1
hohno-46466 <hohno.46466@gmail.com>
2022-06-01 20:55:53 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- scripts/addts.sh
+++ scripts/addts.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+type gdate > /dev/null 2>&1 && alias date=gdate
 [ "x$TSKEY" = "x" ] && TSKEY='$USER-$$'
 cat "$@" | sed -u -e 's/^/echo "'$TSKEY'\t$(date +%s.%3N\)\t/' -e 's/$/"/' | /bin/sh
