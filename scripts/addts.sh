cfeb555322725c93f770d77fca881e4fe515c919
hohno-46466 <hohno.46466@gmail.com>
2022-06-17 21:03:58 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2016B.local

--- scripts/addts.sh
+++ scripts/addts.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-type gdate > /dev/null 2>&1 && alias date=gdate
+export date=date; type gdate > /dev/null 2>&1 && date=gdate # alias date=gdate
+export sed=sed;   type gsed  > /dev/null 2>&1 && sed=gsed   # alias sed=gsed
 [ "x$TSKEY" = "x" ] && TSKEY='$USER-$$'
-cat "$@" | sed -u -e 's/^/echo "'$TSKEY'\t$(date +%s.%3N\)\t/' -e 's/$/"/' | /bin/sh
+cat "$@" | $sed -u -e 's/^/echo "'$TSKEY'\t$($date +%s.%3N\)\t/' -e 's/$/"/' | /bin/sh
